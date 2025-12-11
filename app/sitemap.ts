8334340eab7f022fdabb531588c00757bbb98c1b
Joseph Lau <jlau.dev@gmail.com>
2025-12-11 16:40:59 +0800

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,14 +8,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: baseUrl,
       lastModified: new Date(),
       changeFrequency: "monthly",
-      priority: 1.0,
+      priority: 1,
     },
-    // Add more pages here when you create them:
-    // {
-    //   url: `${baseUrl}/about`,
-    //   lastModified: new Date(),
-    //   changeFrequency: "monthly",
-    //   priority: 0.8,
-    // },
   ];
 }
