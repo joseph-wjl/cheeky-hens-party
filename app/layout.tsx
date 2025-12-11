a139e7da404ca711581b8521fe87f68b4e8cab8a
Joseph Lau <jlau.dev@gmail.com>
2025-12-11 17:07:13 +0800

    Add google GA4 tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,6 @@ const playfairDisplay = Playfair_Display({
   variable: "--font-playfair-display"
 });
 
-
 export const metadata = {
   title: "Cheeky Hens",
   description: "Fun, cheeky, creative hen party life modelling experiences.",
@@ -20,11 +19,31 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={playfairDisplay.variable}>
       <body>
+        {/* FontAwesome */}
         <Script
           src="https://kit.fontawesome.com/d9c9267d74.js"
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+
+        {/* Google Analytics 4 */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-PNTWVS7M6P"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="ga4-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PNTWVS7M6P', { page_path: window.location.pathname });
+            `,
+          }}
+        />
+
         <Navbar />
         <main className="min-h-screen">{children}</main>
         <Footer />
